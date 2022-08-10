2c2b0a0a411329503f92bc9e20c6e0220c885656
AndrianAntonio <andrianantenaina@gmail.com>
2022-08-10 16:48:16 +0300

    core routes

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import '../../presentation/views/breaking_news_page.dart';
 
 class AppRoutes {
   static Route<dynamic>? onGenerateRoutes(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return _materialRoute(const Scaffold());
+        return _materialRoute(const BreakingNewsPage());
       default:
         return null;
     }
